9b02c669747c87d5e86a748cc8cac80df6d9b6de
jaoks <amaruorihuela@gmail.com>
2019-06-17 10:22:16 -0500

    chavez hero programming

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,21 @@
 package com.mycompany.app;
-
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    
+
     public static void main( String[] args )
     {
-        Translator trans = new Translator();
-        String text =  "Hola, mundo!";
-        trans.printTranslation(text);
+      Scanner in = new Scanner(System.in);
+      Translator trans = new Translator();
+      System.out.println("Write the text :");
+      String text =  in.nextLine();
+      trans.showChoices();
+      String code = in.nextLine();
+      trans.printTranslation(text, code);
     }
 }
--- my-app/src/main/java/com/mycompany/app/Translator.java
+++ my-app/src/main/java/com/mycompany/app/Translator.java
@@ -5,30 +5,45 @@ import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.Translate.TranslateOption;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+import com.google.cloud.translate.Language;
+import java.util.List;
 
 public class Translator
 {
 
+
     public Translate translate;
-    
+
+    private static final String key = "AIzaSyBJ01MsImGXDFSe2WdGMdTe-CvevbTUPoc";
+
     public Translator()
     {
-        translate = TranslateOptions.getDefaultInstance().getService();
+        translate = TranslateOptions.newBuilder().setApiKey(key).build().getService();
     }
     private String languageDetected(String text)
     {
         Detection detection = translate.detect(text);
         return detection.getLanguage();
     }
-    
-    public void printTranslation(String inputText)
+
+    public void showChoices()
+    {
+      List<Language> languages = translate.listSupportedLanguages();
+      System.out.println("Choose 1 code of a language");
+      for (Language language : languages) {
+        System.out.printf("Name: %s, Code: %s\n", language.getName(), language.getCode());
+      }
+    }
+
+    public void printTranslation(String inputText, String lang)
     {
         String origin = languageDetected(inputText);
+
         Translation translation = translate.translate(
             inputText,
             TranslateOption.sourceLanguage(origin),
-            TranslateOption.targetLanguage("en"));
+            TranslateOption.targetLanguage(lang));
         System.out.println(translation.getTranslatedText());
     }
-    
+
 }
