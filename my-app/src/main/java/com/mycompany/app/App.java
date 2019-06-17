dbaee3a95181e68d65c721c3d30490cacd568e61
Sergio <sercar88@gmail.com>
2019-06-17 10:29:38 -0500

    forst step unhappy path

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -14,8 +14,32 @@ public class App
       Translator trans = new Translator();
       System.out.println("Write the text :");
       String text =  in.nextLine();
-      trans.showChoices();
-      String code = in.nextLine();
+
+      List<Language> languages = translate.listSupportedLanguages();
+      boolean valid = True;
+
+      while(valid)
+      {
+        trans.showChoices();
+        String code = in.nextLine();
+
+        for (Language language : languages) {
+          
+          if ((language.getCode()).equals(code) ) {
+            valid=False;
+            
+          }
+        }
+
+      }
+
+
       trans.printTranslation(text, code);
     }
 }
+
+
+
+      System.out.println("Choose 1 code of a language");
+      for (Language language : languages) {
+        System.out.printf("Name: %s, Code: %s\n", language.getName(), language.getCode());
